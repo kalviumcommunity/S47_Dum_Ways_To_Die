e37b7211e9fd6a75170a90fa268d3ad2c5219479
rishueee <jhapinki567@gmail.com>
2024-01-25 16:43:23 +0530

    mongoose push

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const URI = 'mongodb+srv://Rishabh:qwertyuiop@cluster0.8yyadat.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(URI)
+.then(()=>{
+  app.get('/',(req,res)=>{
+    res.json({Connection_Status:'Connected'})
+  })
+}).catch((err)=>{
+  app.get('/',(req,res)=>{
+    res.json({Connection_Status:'Not Conncected'})
+  })
+})
 
-app.get('/', (req, res) => {
+app.get('/ping', (req, res) => {
   res.send("Hello Rishabh");
 })
 
 app.listen(5000, ()=> {
   console.log("Server is running on port: 5000")
 })
+
