8c8ec84521eb97672d1454380ae7544310e9e2f5
rishueee <jhapinki567@gmail.com>
2024-02-14 22:54:29 +0530

    filter dropdown added

--- client/src/Component/Login.jsx
+++ client/src/Component/Login.jsx
@@ -1,60 +1,97 @@
-import React, { useState } from 'react';  
-import { useNavigate,Link } from 'react-router-dom'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
+import "./Login.css"
 const Login = () => {
-  const [password,setPassword]=useState('')
-  const [email,setEmail]=useState('')
-  const [name, setName] = useState("");
-    const navigate = useNavigate()
-    const Handlelogin = () =>{
-    navigate("/Main-page")
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
-    axios.post("http://localhost:3000/login",{name,password,email})
-    .then(res =>{
-      const token = res.data.token;
-      document.cookie = `token=${token}; expires=Fri, 07 Aug 2050 00:00:00 GMT;`
-      navigate('/Main-page')
-    })
-   .catch(err => console.log(err))
+  const navigate = useNavigate();
+
+  const Handlelogin = (e) => {
+    e.preventDefault();
+    if (!isValid) return;
+
+    navigate('/Main-page');
+
+    axios
+      .post('http://localhost:3000/login', { name, password, email })
+      .then((res) => {
+        const token = res.data.token;
+        document.cookie = `token=${token}; expires=Fri, 07 Aug 2050 00:00:00 GMT;`;
+        navigate('/Main-page');
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const setcookies = (key, value) => {
+    document.cookie = `${key} = ${value}`;
+  };
+
+  useEffect(() => {
+    if (name === '' || password === '' || email === '') {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
     }
+  }, [name, password, email]);
 
-
-    const setcookies = (key,value) =>{
-        document.cookie = `${key} = ${value}`
-    }
-    
   return (
-    <div>
+    <>
+
+    <div className='body'>
       <form onSubmit={Handlelogin}>
-    <div>
-    <label>Name</label>
-    <input type="text"
-      placeholder='Enter Your Name'
-      required
-      onChange={(e) => {setcookies("Name",e.target.value), setName(e.target.value)}} />
-  </div>
-  <div>
-    <label>Passowrd</label>
-    <input type="password"
-      placeholder='Enter Your Password'
-      required
-      onChange={(e) => {setcookies("Password",e.target.value), setPassword(e.target.value)}} />
-  </div>
-  <div>
-    <label>Email</label>
-    <input type="email"
-      placeholder='Enter Your Email'
-      required
-      onChange={(e) => {setcookies("Email",e.target.value) ,setEmail(e.target.value)}} />
-  </div>
-  </form>
-     <button style={{textAlign:"center",position:"relative",left:"400px",backgroundColor:"green"}} className='back' onClick={Handlelogin}>Submit</button>    
+        <div>
+        <h1>LOGIN</h1>
+          <label>Name</label>
+          <input
+            type="text"
+            placeholder="Enter Your Name"
+            required
+            onChange={(e) => {
+              setcookies('Name', e.target.value);
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label>Passowrd</label>
+          <input
+            type="password"
+            placeholder="Enter Your Password"
+            required
+            onChange={(e) => {
+              setcookies('Password', e.target.value);
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <label>Email</label>
+          <input
+            type="email"
+            placeholder="Enter Your Email"
+            required
+            onChange={(e) => {
+              setcookies('Email', e.target.value);
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+      </form>
     </div>
-  )
-}
+    
+      <button
+     
+        className="back"
+        onClick={Handlelogin}
+      >
+        Submit
+      </button>
+    </>
+  );
+};
 
-export default Login
-
-
-
-
+export default Login;
